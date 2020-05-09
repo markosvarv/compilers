aca0811c206b8b24899044e1ccb3fba53257b5f8
Mark Varvagiannis <markosvarv@gmail.com>
2020-05-09 19:08:31 +0300

    project 3 initial commit

---
+++ project3/LoweringVisitor.java
@@ -0,0 +1,6 @@
+import syntaxtree.*;
+import visitor.GJDepthFirst;
+
+public class LoweringVisitor extends GJDepthFirst<String, String> {
+
+}
---
+++ project3/Main.java
@@ -0,0 +1,51 @@
+import syntaxtree.*;
+import visitor.*;
+import java.io.*;
+
+class Main {
+    public static void main (String [] args){
+        if(args.length == 0){
+            System.err.println("Usage: java [MainClassName] [file1] [file2] ... [fileN]");
+            System.exit(1);
+        }
+        FileInputStream fis = null;
+
+        for (String current_input : args) {
+            //SymbolTable symbol_table = new SymbolTable();
+            System.out.println("\nFile: " + current_input);
+
+            try{
+                fis = new FileInputStream(current_input);
+                MiniJavaParser parser = new MiniJavaParser(fis);
+                LoweringVisitor ll_visitor = new LoweringVisitor();
+                //FillTableVisitor eval = new FillTableVisitor();
+                //TypeCheckingVisitor type_ch = new TypeCheckingVisitor();
+                Goal root = parser.Goal();
+                System.out.println("Program parsed successfully.");
+                try {
+                    root.accept(ll_visitor, "");
+                    //root.accept(eval, symbol_table);
+                    //root.accept(type_ch, symbol_table);
+                    //symbol_table.printOffsets();
+                }catch (Exception exc) {
+                    System.err.println("Error: " + exc.getMessage());
+                }
+            }
+            catch(ParseException ex){
+                System.out.println(ex.getMessage());
+            }
+            catch(FileNotFoundException ex){
+                System.err.println(ex.getMessage());
+            }
+            finally{
+                try{
+                    if(fis != null) fis.close();
+                }
+                catch(IOException ex){
+                    System.err.println(ex.getMessage());
+                }
+            }
+            System.out.println("-------------------------------");
+        }
+    }
+}
